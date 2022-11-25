32baf24a85428c8e93d6eb98dd7791e8ef3df484
HibbaHindawi <89245458+Johanness15@users.noreply.github.com>
2022-11-25 11:03:57 +0100

    Uppgift 1
    Här är mitt kod som inte är klar

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uppgift1_Morötter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur lång tid tar det för Tor att äta en morot? Svara i sekunder, mellan 1-100.");
+            int torsTid = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur lång tid tar det för tors mor att äta en morot? Svara i sekunder, mellan 1-100.");
+            int MorsTid = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
+
+            // Jag fastnade och förstog inte vad jag ska göra;
